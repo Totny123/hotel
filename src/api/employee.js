1319d9677d3a98abdadb4593dc548e9d22dbcba2
liujunfeng <24455370@qq.com>
2022-04-26 05:46:41 +0800

    feat: 员工

---
+++ src/api/employee.js
@@ -0,0 +1,33 @@
+import request from "@/utils/request";
+
+export function getEmployeeList(query) {
+  return request({
+    url: "/employee/list",
+    method: "get",
+    params: query,
+  });
+}
+
+export function delEmployee(data) {
+  return request({
+    url: "/employee/del",
+    method: "post",
+    data,
+  });
+}
+
+export function addEmployee(data) {
+  return request({
+    url: "/employee/add",
+    method: "post",
+    data,
+  });
+}
+
+export function editEmployee(data) {
+  return request({
+    url: "/employee/edit",
+    method: "post",
+    data,
+  });
+}
